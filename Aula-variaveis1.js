ab1b8461efc3719b1eff8149e977a0b65d06fb9d
PedroMachadoGRP <143435304+PedroMachadoGRP@users.noreply.github.com>
2023-09-06 16:19:56 -0300

    Exercicios da aula de variaveis

---
+++ Aula-variaveis1.js
@@ -0,0 +1,20 @@
+//atividade
+
+// 1) crie um programa que peça a um usuario 2 números e mostre a soma 
+const a = prompt("Escreva um número")
+const b = prompt ("escreva um número diferente")
+let c = Number (a)
+let d = Number (b)
+
+console.log("a soma dos seus números é igual á", c + d);
+
+// 2) um programa que diga se um número é par ou impar com true ou false
+
+let num = c % d
+console.log("seu número é par?", num === 0 );
+console.log("seu número é impar", num !==0);
+
+// 3) um programa que calcule o valor vezes ele mesmo
+
+console.log("Seu primeiro número multiplicado por si mesmo é ", c*c);
+console.log("Seu segundo número multiplicado por si mesmo é", d*d);
---
+++ aula-variaveis.js
@@ -0,0 +1,9 @@
+let a = prompt("Escreva um número")
+let b = prompt("escreva um número diferente")
+let c = Number (a)
+let d = Number (b)
+let e = c + d
+let f = c * d 
+
+console.log("A soma dos seu números é",e);
+console.log("A multiplicação dos seus números é", f);
